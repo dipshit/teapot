f3e8d9e0efb9f66dd453ebfa11fa0555ac7db805
Kirin Rastogi <kirin.rastogi@shopify.com>
2019-07-11 18:57:59 -0400

    🐸 Teapot ☕️

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::prelude::*;
+use std::net::{Shutdown, TcpListener, TcpStream};
+use threadpool::ThreadPool;
+
+const TEAPOT: &[u8] = b"HTTP/1.1 418 I'm a teapot\r\n\r\n";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    let pool = ThreadPool::new(12);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle(stream);
+        });
+    }
+}
+
+fn handle(mut stream: TcpStream) {
+    stream.write(TEAPOT).unwrap();
+    stream.shutdown(Shutdown::Both).unwrap();
 }
