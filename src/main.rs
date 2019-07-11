7acbdaf622fb3a0d2eb8a42b1b6ec9d52c8affbb
Kirin Rastogi <kirin.rastogi@shopify.com>
2019-07-11 19:21:29 -0400

    Handle errors and bump threadpool size

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,14 @@ const TEAPOT: &[u8] = b"HTTP/1.1 418 I'm a teapot\r\n\r\n";
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
-    let pool = ThreadPool::new(12);
+    let pool = ThreadPool::new(16);
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        // gracefully handle bad listen
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(_) => continue,
+        };
         pool.execute(|| {
             handle(stream);
         });
@@ -17,6 +21,12 @@ fn main() {
 }
 
 fn handle(mut stream: TcpStream) {
-    stream.write(TEAPOT).unwrap();
-    stream.shutdown(Shutdown::Both).unwrap();
+    match stream.write(TEAPOT) {
+        Ok(_) => (),
+        Err(e) => println!("{}", e),
+    }
+    match stream.shutdown(Shutdown::Both) {
+        Ok(_) => (),
+        Err(e) => println!("{}", e),
+    }
 }
