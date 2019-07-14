a468cde6b78448dcf752c62120397b33012de264
Kirin Rastogi <kirin.rastogi@shopify.com>
2019-07-14 11:44:28 -0400

    Use nonblocking syscalls as a base

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,70 @@
-use std::io::prelude::*;
-use std::net::{Shutdown, TcpListener, TcpStream};
-use threadpool::ThreadPool;
+use libc::{fork, getpid};
+use std::io::{self, Read, Write};
+use std::net::{Shutdown, TcpListener};
 
 const TEAPOT: &[u8] = b"HTTP/1.1 418 I'm a teapot\r\n\r\n";
-const NOTFOUND: &[u8] = b"HTTP/1.1 404 Not Found\r\n\r\n";
-const UNAVAILABLE: &[u8] = b"HTTP/1.1 503 Service Unavailable\r\n\r\n";
-
-const GET: &[u8] = b"GET / HTTP/1.1\r\n";
 
 // Use a listen syscall and handle connections in a threadpool
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
-    let pool = ThreadPool::new(16);
-
+    listener
+        .set_nonblocking(true)
+        .expect("Cannot set listener non-blocking");
     println!("Teapot starting on port 8080");
 
-    for stream in listener.incoming() {
-        // gracefully handle bad listen
-        let stream = match stream {
-            Ok(stream) => stream,
-            Err(_) => continue,
-        };
-        pool.execute(|| {
-            handle(stream);
-        });
-    } // socket closed
-}
-
-fn handle(mut stream: TcpStream) {
-    // only read the first 512 bytes
-    let mut buffer = [0; 512];
-    match stream.read(&mut buffer) {
-        Ok(_) => (),
-        // respond 503 if out of file descriptors
-        Err(_) => {
-            send(UNAVAILABLE, stream);
-            return;
+    unsafe {
+        let parent_pid = getpid();
+        println!("parent is {}", parent_pid);
+        for _ in 1..16 {
+            if getpid() == parent_pid {
+                fork();
+            }
         }
     }
-    if buffer.starts_with(GET) {
-        return send(TEAPOT, stream);
-    }
-    // issue Not Found instead of 400 Bad Request so we don't have
-    // to parse the request
-    send(NOTFOUND, stream);
+    run(&listener);
 }
 
-fn send(response: &[u8], mut stream: TcpStream) {
-    match stream.write(response) {
-        Ok(_) => (),
-        Err(e) => println!("{}", e),
+// have all procs epoll_create and block on it
+fn run(listener: &TcpListener) {
+    unsafe {
+        println!("pid {} ready", getpid());
     }
-    // prevent TCP RST if client sent req longer than 512 bytes
-    match stream.shutdown(Shutdown::Both) {
-        Ok(_) => (),
-        Err(e) => println!("{}", e),
-    }
+    // accept in a loop
+    for stream in listener.incoming() {
+        let mut stream = match stream {
+            Ok(stream) => stream,
+            // drop connection if out of file descriptors
+            Err(_) => continue,
+        };
+        stream
+            .set_nonblocking(true)
+            .expect("Cannot set stream non-blocking");
+
+        // wait until socket is done being written to before writing
+        // this helps avoid TCP RST if we read non blocking
+        let mut buffer = [0];
+        match stream.read(&mut buffer) {
+            Ok(_) => (),
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                println!("Read would block: {}", e);
+                continue;
+            }
+            Err(e) => println!("{}", e),
+        }
+
+        match stream.write(TEAPOT) {
+            Ok(_) => (),
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                println!("Write would block: {}", e);
+                continue;
+            }
+            Err(e) => println!("{}", e),
+        }
+
+        // shutdown to prevent TCP RST
+        match stream.shutdown(Shutdown::Both) {
+            Ok(_) => (),
+            Err(e) => println!("{}", e),
+        }
+    } // close socket
 }
